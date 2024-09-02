1e3586858072406685eed6a945afad487f258b42
Luciano Germani <lucianogermani93@gmail.com>
2024-09-02 13:33:02 +0200

    Fix: mobile hero visible

--- src/app/[locale]/components/hero/index.jsx
+++ src/app/[locale]/components/hero/index.jsx
@@ -1,31 +1,60 @@
+'use client'
 import { useTranslations }      from "next-intl";
 import Image from 'next/image';
 import drink from "../../../../static/media/img/drink.jpg";
+import { useSelector }      from 'react-redux';
 import { HiOutlineArrowLongRight } from "react-icons/hi2";
 import './index.scss';
 
 const Hero = () => {
   const t = useTranslations("Hero");
+  const isMobile = useSelector(state => state.responsive.isMobile);
   return (
       <div className="hero-design">
-        <div className="image-container">
-        <Image className="img" src={drink} alt={'drink'} width={700} height={900}  quality={100}  />
-        </div>
-        <div className="text-section">
-          <div className="text">
-            <h1>{t('our')}</h1>
-            <h4>{t('kitchen')}</h4>
+        {
+          isMobile ?
+          <>
+           <div className="text">
+              <h1>{t('our')}</h1>
+              <h4>{t('kitchen')}</h4>
+            </div>
+
+          <div className="image-container">
+            <Image className="img" src={drink} alt={'drink'}   quality={100}  />
           </div>
+
           <div className="bottom-text">
-            <p className="capital-letter">{t('capital-letter')}</p>
-            <p>{t('text1')}</p>
-            <div className="button-menu">
-              <p className="menu">{t('menu')}</p>
-              <HiOutlineArrowLongRight className="arrow" size={40}/>
+              <p className="capital-letter">{t('capital-letter')}</p>
+              <p>{t('text1')}</p>
+              <div className="button-menu">
+                <p className="menu">{t('menu')}</p>
+                <HiOutlineArrowLongRight className="arrow" size={40}/>
+              </div>
+           </div>
+
+          </>
+          :
+          <>
+          <div className="image-container">
+          <Image className="img" src={drink} alt={'drink'} width={700} height={900}  quality={100}  />
+          </div>
+          <div className="text-section">
+            <div className="text">
+              <h1>{t('our')}</h1>
+              <h4>{t('kitchen')}</h4>
+            </div>
+            <div className="bottom-text">
+              <p className="capital-letter">{t('capital-letter')}</p>
+              <p>{t('text1')}</p>
+              <div className="button-menu">
+                <p className="menu">{t('menu')}</p>
+                <HiOutlineArrowLongRight className="arrow" size={40}/>
+              </div>
             </div>
           </div>
+          </>
+        }
         </div>
-      </div>
 
     )
 }
