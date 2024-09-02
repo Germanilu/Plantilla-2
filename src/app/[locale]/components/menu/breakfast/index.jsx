5c045e699bd0006299d04715c41356a591e03737
Luciano Germani <lucianogermani93@gmail.com>
2024-09-02 17:58:11 +0200

    Feat: brk imgs

--- src/app/[locale]/components/menu/breakfast/index.jsx
+++ src/app/[locale]/components/menu/breakfast/index.jsx
@@ -1,11 +1,26 @@
+import { HiOutlineArrowLongRight } from "react-icons/hi2";
 import Image from 'next/image';
-import drink from "../../../../../static/media/img/drink.jpg";
+import porridge from "../../../../../static/media/img/porridge.jpg";
+import restaurant from "../../../../../static/media/img/restaurant.jpg";
+import breakfast from "../../../../../static/media/img/breakfast.jpg";
 import './index.scss';
 const Breakfast = () => {
     return(
         <div className='breakfast-design'>
-            <Image className="img" src={drink} alt={'drink'} width={700} height={900}  quality={100}  />
-            hola
+            <></>
+            <div className="img-left">
+                <div className="text">
+                    <p>breakfast with us</p>
+                    <HiOutlineArrowLongRight className="arrow" size={40}/>
+                </div>
+                <Image className="img" src={porridge} alt={'drink'} width={766} height={468}  quality={100}  />
+            </div>
+            <div className="img-right">
+                <Image className="img" src={breakfast} alt={'drink'} width={468} height={766}  quality={100}  />
+            </div>
+            <div className="img-bottom">
+                <Image className="img" src={restaurant} alt={'drink'} width={950} height={450}  quality={100}  />
+            </div>
         </div>
     )
 }
