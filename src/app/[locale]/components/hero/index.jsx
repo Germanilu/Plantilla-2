8f19b70129e272a9e8c28b5b91e6fe2cd0d034ea
Luciano Germani <lucianogermani93@gmail.com>
2024-09-02 14:09:13 +0200

    Fix: mobile hero, motion framer

--- src/app/[locale]/components/hero/index.jsx
+++ src/app/[locale]/components/hero/index.jsx
@@ -1,8 +1,9 @@
 'use client'
+import { useSelector }      from 'react-redux';
 import { useTranslations }      from "next-intl";
 import Image from 'next/image';
 import drink from "../../../../static/media/img/drink.jpg";
-import { useSelector }      from 'react-redux';
+import {motion} from "framer-motion"
 import { HiOutlineArrowLongRight } from "react-icons/hi2";
 import './index.scss';
 
@@ -15,42 +16,132 @@ const Hero = () => {
           isMobile ?
           <>
            <div className="text">
-              <h1>{t('our')}</h1>
-              <h4>{t('kitchen')}</h4>
+              <motion.h1
+              variants={{
+                hidden:{opacity:0, x:75},
+                visible:{opacity:1,x:0},
+              }}
+              transition={{delay: 0.5, duration:0.5}}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              >{t('our')}</motion.h1>
+              <motion.h4
+               variants={{
+                hidden:{opacity:0, x:-75},
+                visible:{opacity:1,x:0},
+              }}
+              transition={{delay: 0.5, duration:0.5}}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              >{t('kitchen')}</motion.h4>
             </div>
 
-          <div className="image-container">
+          <motion.div className="image-container"
+           variants={{
+            hidden:{opacity:0, y:75},
+            visible:{opacity:1,y:0},
+          }}
+          transition={{delay: 0.7, duration:0.5}}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          >
             <Image className="img" src={drink} alt={'drink'}   quality={100}  />
-          </div>
+          </motion.div>
 
-          <div className="bottom-text">
+          <motion.div className="bottom-text"
+          variants={{
+            hidden:{opacity:0, y:75},
+            visible:{opacity:1,y:0},
+          }}
+          transition={{delay: 1, duration:0.5}}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          >
               <p className="capital-letter">{t('capital-letter')}</p>
               <p>{t('text1')}</p>
-              <div className="button-menu">
+              <motion.div className="button-menu"
+               variants={{
+                hidden:{opacity:0, x:-75},
+                visible:{opacity:1,x:0},
+              }}
+              transition={{delay: .3, duration:0.5}}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              >
                 <p className="menu">{t('menu')}</p>
                 <HiOutlineArrowLongRight className="arrow" size={40}/>
-              </div>
-           </div>
+              </motion.div>
+           </motion.div>
 
           </>
           :
           <>
-          <div className="image-container">
-          <Image className="img" src={drink} alt={'drink'} width={700} height={900}  quality={100}  />
-          </div>
+          <motion.div className="image-container"
+           variants={{
+            hidden:{opacity:0, x:-75},
+            visible:{opacity:1,x:0},
+          }}
+          transition={{delay: 0.7, duration:0.5}}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          >
+            <Image className="img" src={drink} alt={'drink'} width={700} height={900}  quality={100}  />
+          </motion.div>
           <div className="text-section">
             <div className="text">
-              <h1>{t('our')}</h1>
-              <h4>{t('kitchen')}</h4>
+              <motion.h1
+                variants={{
+                  hidden:{opacity:0, x:75},
+                  visible:{opacity:1,x:0},
+                }}
+                transition={{delay: 0.5, duration:0.5}}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+              >{t('our')}</motion.h1>
+              <motion.h4
+               variants={{
+                hidden:{opacity:0, x:-75},
+                visible:{opacity:1,x:0},
+              }}
+              transition={{delay: 0.5, duration:0.5}}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              >{t('kitchen')}</motion.h4>
             </div>
-            <div className="bottom-text">
+            <motion.div className="bottom-text"
+             variants={{
+              hidden:{opacity:0, x:0},
+              visible:{opacity:1,x:0},
+            }}
+            transition={{delay: 1, duration:0.5}}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            >
               <p className="capital-letter">{t('capital-letter')}</p>
               <p>{t('text1')}</p>
-              <div className="button-menu">
+              <motion.div className="button-menu"
+                variants={{
+                  hidden:{opacity:0, x:-75},
+                  visible:{opacity:1,x:0},
+                }}
+                transition={{delay: 1.3, duration:0.5}}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                >
                 <p className="menu">{t('menu')}</p>
                 <HiOutlineArrowLongRight className="arrow" size={40}/>
-              </div>
-            </div>
+              </motion.div>
+            </motion.div>
           </div>
           </>
         }
