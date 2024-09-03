e941b40a09456ae6ef14208f5aaac22eb9aad114
Luciano Germani <lucianogermani93@gmail.com>
2024-09-03 16:51:47 +0200

    Feat: Footer

--- src/app/[locale]/components/footer/index.jsx
+++ src/app/[locale]/components/footer/index.jsx
@@ -1,5 +1,6 @@
+'use client';
 import { useTranslations }      from "next-intl";
-
+import {motion} from "framer-motion";
 import './index.scss'
 
 export default function Footer(){
@@ -7,9 +8,35 @@ export default function Footer(){
   const t = useTranslations("Footer");
 
   return (
-    <div className="footer-design">
-      <h1>{t('title')}</h1>
-    </div>
+    <motion.div className="footer-design"
+    variants={{
+      hidden:{opacity:0, y:0},
+      visible:{opacity:1,y:0},
+    }}
+    transition={{delay:  1.5, duration:0.5}}
+    initial="hidden"
+    whileInView="visible"
+    viewport={{ once: true }}
+    >
+      <div className="container-info">
+        <div className="info">
+          <h2 className="info-label">{t('phone-label')}</h2>
+          <p  className="info-text">{t('phone-number')}</p>
+        </div>
+        <div className="info">
+          <h2  className="info-label">{t('inquiries-label')}</h2>
+          <p  className="info-text">{t('inquiries-email')}</p>
+        </div>
+        <div className="info socials">
+          <h2 className="info-label">{t('socials-label')}</h2>
+          <span  className="info-text">{t('instagram')}</span>
+          <span  className="info-text">{t('facebook')}</span>
+        </div>
+      </div>
+      <div className="registered">
+        <span  className="info-text">{t('text')}</span>
+      </div>
+    </motion.div>
   ) 
 }
 
