482cfa16c41c9aa1b7d8150d35c3cbaf3b161f8b
Luciano Germani <lucianogermani93@gmail.com>
2024-09-03 14:04:27 +0200

    Feat: Complete mobile and animation contact component

--- src/app/[locale]/components/contact/index.jsx
+++ src/app/[locale]/components/contact/index.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useRef, useState }        from "react";
-import emailjs                            from "@emailjs/browser";
+import {motion} from "framer-motion";
 import { useTranslations }                from "next-intl";
 
 import './index.scss';
@@ -10,7 +10,16 @@ export default function Contact() {
   const t = useTranslations('Contact-Component');
   
   return(
-    <div className="contact-design">
+    <motion.div className="contact-design"
+    variants={{
+      hidden:{opacity:0, y:75},
+      visible:{opacity:1,y:0},
+    }}
+    transition={{delay:  0.7, duration:0.7}}
+    initial="hidden"
+    whileInView="visible"
+    viewport={{ once: true }}>
+      
       <h2>{t('title')}</h2>
       <div className="contact-information-box">
         <div className="info">
@@ -22,17 +31,17 @@ export default function Contact() {
           <p className="p-info">{t('address')}</p>
         </div>
         <div className="info">
-          <p className="p-principal">{t('contact-label')}</p>
-          <p className="p-info">{t('email')}</p>
-          <p className="p-info">{t('phone')}</p>
-        </div>
-        <div className="info">
           <p className="p-principal">{t('hours-label')}</p>
           <p className="p-info">{t('weekday-hours')}</p>
           <p className="p-info">{t('weekend-hours')}</p>
           <p className="p-info">{t('monday-closed')}</p>
         </div>
+        <div className="info">
+          <p className="p-principal">{t('contacts-label')}</p>
+          <p className="p-info">{t('email')}</p>
+          <p className="p-info">{t('phone')}</p>
+        </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
